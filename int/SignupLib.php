d78528016ebfa97f5715532b401769905330107b
rjp <richard@wavwebs.com>
2018-01-25 16:20:41 +0000

    Campsite manage

--- int/SignupLib.php
+++ int/SignupLib.php
@@ -1,6 +1,6 @@
 <?php
 
-$lnlclasses = array('','Live and Louder (under 16s)','Live and Loud (17-44)','Still Live and Loud (45+)');
+$lnlclasses = array('','Live and Louder (under 16s)','Live and Loud (17-44)','Still Alive and Loud (45+)');
 $Colours = array('white','lime','orange','grey');
 $yesno = array('','Yes','No');
 $States = array('Submitted','Paid','Cancelled');
