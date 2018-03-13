9cb23074c81665212cc6e8cf5f8828592d3b4202
rjp <richard@wavwebs.com>
2018-03-13 09:24:29 +0000

    Final tweak...

--- int/ShowDanceProg.php
+++ int/ShowDanceProg.php
@@ -25,6 +25,7 @@
     Scan_Data($Cond);
     if ($Public) echo "<p><h2>Saturday Dance</h2><p>\n";
     echo "This will be easier to use on a small screen soon.<p>";
+    echo "Click on a team to learn more about them, click on a venue to find out where it is.<p>";
     Create_Grid($Cond);
     Print_Grid(0,0,$Cond,$Public);
   }
@@ -34,6 +35,7 @@
       Scan_Data($Cond);
       echo "<p><h2>Sunday Dance</h2></p>\n";
       echo "This will be easier to use on a small screen soon.<p>";
+      echo "Click on a team to learn more about them, click on a venue to find out where it is.<p>";
       Create_Grid($Cond);
       Print_Grid(0,0,$Cond,$Public);
       dotail();
