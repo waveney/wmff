f2b10b7a183f59dc3f4d7afe3794b4c7a0ee0936
rjp <richard@wavwebs.com>
2018-04-24 17:47:22 +0100

    Ban ads take 3

--- js/WmffAds.js
+++ js/WmffAds.js
@@ -3,13 +3,25 @@ function setHeadRow() {
   var ww = window.innerWidth;
   var scale = Math.floor(ww*250/(550*2+864+20));
   if (scale>250) scale=250;
-  if (ww > 800) $('#leftspon').removeAttr('hidden'); 
-  if (ww > 1200) $('#rightspon').removeAttr('hidden');
+
+  if (ww < 1000) {
+    if (ww < 500) {
+      scale = 250
+    } else {
+      scale = Math.floor(ww*250/(550+864+25));
+    }
+  }
 
   $('#HeadRow').css('max-height',scale);
   $('#leftspon').css('max-height',scale);
   $('#rightspon').css('max-height',scale);
   $('#HeadBan').css('max-height',scale);
+
+  $('#leftspon').hide();
+  $('#rightspon').hide();
+  
+  if (ww > 500) $('#leftspon').show();
+  if (ww > 1000) $('#rightspon').show();
 }
 
 $(document).ready(function() {
