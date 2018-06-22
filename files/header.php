23295807e1d67c882d766214e2a631773cf7825d
rjp <richard@wavwebs.com>
2018-06-22 08:32:49 +0100

    Comedy Gallery

--- files/header.php
+++ files/header.php
@@ -1,5 +1,6 @@
-    <meta name="description" content="Wimborne's annual folk festival takes place in the historic market town of Wimborne Minster in Dorset on the weekend of 8, 9, 10 June 2018.">
-    <meta name="keywords" content="wimborne, minster, folk, festival, folk festival, dorset, folkie, fringe, paddock, morris, dance, side, music, concerts, camping, parking, trade, trading, stewards, volunteer, tickets, line up">
+    <meta name="description" content="Wimborne's annual folk festival takes place in the historic market town of Wimborne Minster in Dorset on the weekend of 14, 15, 16 June 2019.">
+    <meta name="keywords" content="wimborne, minster, folk, festival, folk festival, dorset, folkie, fringe, paddock, morris, dance, side, music, concerts, camping, 
+	parking, trade, trading, stewards, volunteer, tickets, line up, appalachian, ceildihs, procession, step dance, workshops, craft, sessions">
     <meta name="copyright" content="Copyright &copy; Wimborne Minster Folk Festival <?php date_default_timezone_set('GMT'); echo date('Y'); ?>"> 
     <meta name="Charset" content="US-ASCII">
     <meta name="Distribution" content="Global">
@@ -16,10 +17,8 @@
     echo "<link href=/files/dropdown.css?V=$V type=text/css rel=stylesheet />\n";
 ?>
 
-<!--    <link href="/css/responsiveslides.css" type="text/css" rel="stylesheet" /> -->
     <script src="/js/jquery-3.2.1.min.js"></script>
     <link href="/files/themes.css" type="text/css" rel="stylesheet" />
     <script src="/js/lightbox.js"></script>
     <link href="/css/lightbox.css" rel="stylesheet" />
 
-<!--    <script src="/js/responsiveslides.min.js"></script> -->
