fdf5d6f853e753d693eaf7aa3b1454ccef4e6c68
rjp <richard@wavwebs.com>
2018-08-17 08:24:36 +0100

    Test SMTP on stage

--- int/TestEmail.php
+++ int/TestEmail.php
@@ -4,6 +4,7 @@
 
   dostaffhead("Test Email");
   include_once("UserLib.php");
+  include_once("Email.php");
 
   if (isset($_GET['U'])) {
     $uid = $_GET['U'];
@@ -13,7 +14,7 @@
         "This is yet another test message ...<p>".
         "Richard";
  
-    SendEmail($user['Email'],"Test From " . firstword($user['SName']) . " from WMFF Staff pages",$letter);
+    NewSendEmail($user['Email'],"Test From " . firstword($user['SName']) . " from WMFF Staff pages",$letter);
 
     echo "Email sent:<p>$letter";
   } else {
