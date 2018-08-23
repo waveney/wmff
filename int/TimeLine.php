ea80a08a0a3a7dc0f54fc48bbd25194d6ed2fe4c
rjp <richard@wavwebs.com>
2018-08-23 19:06:23 +0100

    Assigned to ALL

--- int/TimeLine.php
+++ int/TimeLine.php
@@ -176,8 +176,7 @@ Coming ...
       $Open = 0;
       if (!isset($tl['Start']) || $tl['Start'] == 0) $tl['Start'] = $now;
       if ($tl['Assigned']>0) {
-        if ($tl['Assigned'] != $USERID) { $classes .=  "TL_EVERYONE "; $hide=1; }
-        if ($tl['Assigned'] == 3) { $hide=0; }
+        if ($tl['Assigned'] != $USERID && ($tl['Assigned'] != 3)) { $classes .=  "TL_EVERYONE "; $hide=1; }
       }
       
       if ($tl['Progress'] >= 0) {
