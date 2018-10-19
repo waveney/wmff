f23a77793eb896eff1c0f09dd870da815da69dcd
rjp <richard@wavwebs.com>
2018-10-19 17:10:19 +0100

    Dev Code escaped..

--- int/ImageLib.php
+++ int/ImageLib.php
@@ -77,6 +77,7 @@ function Image_Validate($img) {
 
 function Localise_Image(&$data,$field='Photo') { // If not local, get image store it locally find image size and record its size
   $src = $data[$field];
+
       if (preg_match('/^https?:\/\//i',$side['Photo'])) {
         $stuff = getimagesize($side['Photo']);
       } else if (preg_match('/^\/(.*)\?.*/',$side['Photo'],$mtch)) {
@@ -91,7 +92,8 @@ function Localise_Image(&$data,$field='Photo') { // If not local, get image stor
         $ht = $stuff[1];
         $side['ImageHeight'] = $ht;
         $side['ImageWidth'] = $wi;
-
+        }
+        
 }
 
 function Get_Gallery_Names() {
