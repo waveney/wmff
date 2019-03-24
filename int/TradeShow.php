b9f158dc4754096965f44e2ebd1e14f23cfb1918
rjp <richard@wavwebs.com>
2019-03-24 21:52:03 +0000

    Traders improvement

--- int/TradeShow.php
+++ int/TradeShow.php
@@ -80,6 +80,45 @@
   echo "To become a trader see the <a href=/info/trade>trade info page</a>.  ";
   echo "Only those traders who have paid their deposits and have asked to be listed are shown here.<p>";
 
+  if (Access('SysAdmin')) {
+  
+  echo "<div id=flex>\n";
+  foreach($List as $ti) {
+    $trad = $Traders[$ti];
+ //var_dump($ti,$trad);
+    echo "<div class=TradeFlexCont>";
+    if ($trad['Website']) echo weblinksimple($trad['Website']);
+    echo "<h2>" . $trad['SN'] . "</h2>";
+    if ($trad['Photo']) echo "<img src=" . $trad['Photo'] . ">";
+    if ($trad['Website']) echo "</a>";
+    
+    $txt = $trad['GoodsDesc'];
+    $txt = preg_replace("/\n\n/","<p>\n\n",$txt);
+    echo "<div class=Tradetext>$txt</div>"; // TODO Handle non html chars also do double nl to p
+    
+    if (!$SLoc) {
+      echo ($YEAR >= $PLANYEAR?"<p>Will be trading ":"<p>Was trading ") . $Prefixes[$Locs[$trad['PitchLoc0']]['prefix']] . ' ' . $Locs[$trad['PitchLoc0']]['SN'];
+      if ($trad['PitchLoc2']) {
+        echo ", " . $Prefixes[$Locs[$trad['PitchLoc1']]['prefix']] . ' ' . $Locs[$trad['PitchLoc1']]['SN'] . " and " 
+         		. $Prefixes[$Locs[$trad['PitchLoc1']]['prefix']] . ' ' . $Locs[$trad['PitchLoc2']]['SN'];
+      } else if ($trad['PitchLoc1']) {
+        echo " and " . $Prefixes[$Locs[$trad['PitchLoc1']]['prefix']] . ' ' . $Locs[$trad['PitchLoc1']]['SN'];
+      }
+      if ($trad['Days']) echo " on " . $Trade_Days[$trad['Days']];
+    } else {
+      if ($trad['Days']) echo "On " . $Trade_Days[$trad['Days']];    
+    }
+    echo "</div>";
+  }
+  echo "</div>";
+//  dotail();
+//  exit;  
+  
+  echo "<br clear=all>";
+  echo "<h2>Old Version</h2>";    
+  }
+  // Old Code
+  
   echo "<div id=flex>\n";
   foreach($List as $ti) {
     $trad = $Traders[$ti];
