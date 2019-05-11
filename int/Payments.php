9d05aafecc281e512574ba6298a6c9837133f96d
rjp <richard@wavwebs.com>
2019-05-11 14:35:59 +0100

    Payments stage 2

--- int/Payments.php
+++ int/Payments.php
@@ -6,39 +6,58 @@
   
   A_Check('Committee','Finance');
 
-  dostaffhead("All Performer Payments");
+  $csv = 0;
+  if (isset($_GET['F'])) $csv = $_GET['F'];
+
+  if ($csv) {
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename=PerformerPayments.csv');
+
+    // create a file pointer connected to the output stream
+    $output = fopen('php://output', 'w');
+
+  } else {
+    dostaffhead("All Performer Payments");
+  }
   
   global $db,$YEAR;
   
   $qry = "SELECT s.*, y.* FROM Sides s, SideYear y WHERE y.Year=$YEAR AND y.TotalFee>0 AND s.SideId=y.SideId AND (y.Coming=2 OR y.Yearstate>=2 ) ORDER BY s.SN";
-//  echo $qry;
   $pays = $db->query($qry);
   if (!$pays) { 
     echo "Nothing to pay";
     dotail();
   }
   
-  $coln = 0;
-  echo "<div class=tablecont><table id=indextable border>\n";
-  echo "<thead><tr>";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>id</a>\n";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Name</a>\n";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'N')>Total Fee</a>\n";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Sort Code</a>\n";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Ac Number</a>\n";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Ac Name</a>\n";
+  if ($csv) {
+    $heads = ['Name','Total Fee','Sort Code','Ac Number','Ac Name'];
+    foreach($BUDGET as $i=>$b) {
+      if ($b['id']) $heads[] = $b['SN'];
+    }
+    $heads[] = 'Homeless';
+    
+    fputcsv($output, $heads);
 
-  foreach($BUDGET as $i=>$b) {
-    if ($b['id']) echo "<th><a href=javascript:SortTable(" . $coln++ . ",'N')>" . $b['SN'] . "</a>\n";
+  } else {  
+    echo "<h2><a href=Payments.php?Y=$YEAR&F=CSV>Output as CSV</a></h2>";
+    $coln = 0;
+    echo "<div class=tablecont><table id=indextable border>\n";
+    echo "<thead><tr>";
+    echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>id</a>\n";
+    echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Name</a>\n";
+    echo "<th><a href=javascript:SortTable(" . $coln++ . ",'N')>Total Fee</a>\n";
+    echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Sort Code</a>\n";
+    echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Ac Number</a>\n";
+    echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Ac Name</a>\n";
+
+    foreach($BUDGET as $i=>$b) {
+      if ($b['id']) echo "<th><a href=javascript:SortTable(" . $coln++ . ",'N')>" . $b['SN'] . "</a>\n";
+    }
+    echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Homeless</a>\n";
+    echo "</thead><tbody>";
   }
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Homeless</a>\n";
-  echo "</thead><tbody>";
   
   while ($payee = $pays->fetch_assoc()) {
-    echo "<tr><td>" . $payee['syId'] . "<td>" . $payee['SN'];
-    echo "<td>" . $payee['TotalFee'];
-    echo "<td>" . $payee['SortCode'] . "<td>" . $payee['Account'] . "<td>" . $payee['AccountName'];
-    
     $bud = [];
     $bud[$payee['BudgetArea']] = $payee['TotalFee'];
     if ($payee['BudgetArea2']) {
@@ -49,15 +68,31 @@
       $bud[$payee['BudgetArea3']] = $payee['BudgetValue3'];
       $bud[$payee['BudgetArea']] -= $payee['BudgetValue3'];
     }
-    
-    foreach($BUDGET as $i=>$b) {
-      echo "<td>";
-      if (isset($bud[$i])) echo $bud[$i];
-    }
-    echo "\n";
-  }
-  echo "</table></div>";
-    
-  dotail();
 
+    if ($csv) {
+      $data = [$payee['SN'],$payee['TotalFee'], $payee['SortCode'], $payee['Account'], $payee['AccountName']];
+
+      foreach($BUDGET as $i=>$b)  $data[]= (isset($bud[$i])?$bud[$i]:"");
+      fputcsv($output,$data);
+    } else {
+      echo "<tr><td>" . $payee['SideId'] . "/" . $payee['syId'] . "<td>" . $payee['SN'];
+      echo "<td>" . $payee['TotalFee'];
+      echo "<td>" . $payee['SortCode'] . "<td>" . $payee['Account'] . "<td>" . $payee['AccountName'];
+    
+      foreach($BUDGET as $i=>$b) {
+        echo "<td>";
+        if (isset($bud[$i])) echo $bud[$i];
+      }
+      echo "\n";
+    }
+  }
+  
+  if ($csv) {
+  
+  } else {
+    echo "</table></div>";
+    
+    dotail();
+  }
+?>
 
