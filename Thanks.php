f678ce9346399a5a2226e4ac65a4c18ae89bf89d
rjp <richard@wavwebs.com>
2019-05-23 16:04:38 +0100

    More Buses and Trade Tweak for Moe

--- Thanks.php
+++ Thanks.php
@@ -3,7 +3,7 @@
 
   dohead("Thank You",[],1);
 
-  echo "<center><h3>Thank you for supporting the Wimborne Minster Folk Festival.</h3><h3>This festival would not be possible without the generosity of our supporters</h3>";
+  echo "<center><h3>Thank you for supporting Wimborne Minster Folk Festival.</h3><h3>This festival would not be possible without the generosity of our supporters</h3>";
   
   echo "<img src='https://wimbornefolk.co.uk/int/images/gallery/2018/Around/PSX_20180611_202947.jpg'></center>";
   dotail()
