f02d8c93ad819c7b1cfb269e8734ccf67544063c
rjp <richard@wavwebs.com>
2019-06-02 08:03:35 +0100

    Yellow Buses

--- InfoGettingHere.php
+++ InfoGettingHere.php
@@ -39,7 +39,7 @@ The most convienent train station is <strong>Poole</strong>, with train services
 <a href="https://www.southwesternrailway.com" target="_blank">South Western Railway</a>.<p>
 
 <a name=Buses></a>Regular buses service Wimborne during the folk festival from Poole and Bournemouth. Bus services are operated by 
-<a href="http://morebus.co.uk" target="_blank">More Buses</a> (routes 3, 4 &amp; 13).<p>
+<a href="http://morebus.co.uk" target="_blank">More Buses</a> (routes 3, 4 &amp; 13) and <a href="https://www.yellowbuses.co.uk" target="_blank">Yellow Buses</a> (route 6). <p>
 
 Route 3 runs every 30 minutes to and from <b>Poole</b> to <b>Wimborne</b> Monday to Saturday daytime (and is faster).<p>
 Route 4 runs every 30 minutes to and from <b>Poole</b> to <b>Wimborne</b> every day (but is slower). <p>
@@ -47,30 +47,38 @@ Note: Only half of Route 4 Buses go as far as Wimborne, only those that do, are 
 
 Route 13 goes to and from <b>Bournemouth</b> to <b>Wimborne</b> every 30 minutes Monday to Saturday daytime and hourly on Sunday and the evening.<p>
 
+Route 6 goes to and from <b>Bournemouth</b> to <b>Wimborne</b> every 30 minutes Monday to Saturday daytime (only).<p>
+
 Note there will be a temporary bus stop by the Campsite, this is on the route of the 3, and the 4's that go via Corfe Mullen in the evenings and on Sunday.<p>
 
 Buses in <span class=ExtraBus>Bold Blue</span> are additional buses being run for the festival.<p>
 <div class=tablecont>
 <table class=InfoTable>
-<tr><td>Route<td colspan=3>To Wimborne<td colspan=3>From Wimborne
-<tr><td>Route 3<td><a onclick=ShowBus('3.1') class=FakeLink>Friday</a>
+<tr><td>Route<td>Operator<td colspan=3>To Wimborne<td colspan=3>From Wimborne
+<tr><td>Route 3<td>More<td><a onclick=ShowBus('3.1') class=FakeLink>Friday</a>
 		<td><a onclick=ShowBus('3.2') class=FakeLink>Saturday</a>
 		<td>
                 <td><a onclick=ShowBus('3.4') class=FakeLink>Friday</a>
 		<td><a onclick=ShowBus('3.5') class=FakeLink>Saturday</a>
 		<td>
-<tr><td>Route 4<td><a onclick=ShowBus('4.1') class=FakeLink>Friday</a>
+<tr><td>Route 4<td>More<td><a onclick=ShowBus('4.1') class=FakeLink>Friday</a>
 		<td><a onclick=ShowBus('4.2') class=FakeLink>Saturday</a>
 		<td><a onclick=ShowBus('4.3') class=FakeLink>Sunday</a>
                 <td><a onclick=ShowBus('4.4') class=FakeLink>Friday</a>
 		<td><a onclick=ShowBus('4.5') class=FakeLink>Saturday</a>
 		<td><a onclick=ShowBus('4.6') class=FakeLink>Sunday</a>
-<tr><td>Route 13<td><a onclick=ShowBus('13.1') class=FakeLink>Friday</a>
+<tr><td>Route 13<td>More<td><a onclick=ShowBus('13.1') class=FakeLink>Friday</a>
 		<td><a onclick=ShowBus('13.2') class=FakeLink>Saturday</a>
 		<td><a onclick=ShowBus('13.3') class=FakeLink>Sunday</a>
                 <td><a onclick=ShowBus('13.4') class=FakeLink>Friday</a>
 		<td><a onclick=ShowBus('13.5') class=FakeLink>Saturday</a>
 		<td><a onclick=ShowBus('13.6') class=FakeLink>Sunday</a>
+<tr><td>Route 6<td>Yellow<td><a onclick=ShowBus('6.1') class=FakeLink>Friday</a>
+		<td><a onclick=ShowBus('6.2') class=FakeLink>Saturday</a>
+		<td>
+                <td><a onclick=ShowBus('6.4') class=FakeLink>Friday</a>
+		<td><a onclick=ShowBus('6.5') class=FakeLink>Saturday</a>
+		<td>
 </table></div><p>
 
 <div id=TimeTab></div>
