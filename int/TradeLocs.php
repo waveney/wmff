c815c8f2cd8a24c29af476334945d929176b58f2
rjp <richard@wavwebs.com>
2020-03-08 19:44:52 +0000

    Simple Trade Locs Page

--- int/TradeLocs.php
+++ int/TradeLocs.php
@@ -8,6 +8,7 @@
   include_once("TradeLib.php");
   include_once("InvoiceLib.php");
   
+  echo "<button class='floatright FullD' onclick=\"($('.FullD').toggle())\">All Locs</button><button class='floatright FullD' hidden onclick=\"($('.FullD').toggle())\">Curent Locs</button> ";
   echo "<div class='content'><h2>Manage Trade Locations</h2>\n";
   
   echo "Artisan Messages trigger local Artisan related emails<p>Only set the Invoice Code for locations that override normal trade type invoice codes<p>";
@@ -32,9 +33,9 @@
   echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Has Power</a>\n";
   echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>No List</a>\n";
   echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>In Use</a>\n";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Days</a>\n";
+  echo "<th class=FullD hidden><a href=javascript:SortTable(" . $coln++ . ",'T')>Days</a>\n";
   echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Artisan Msgs</a>\n";
-  echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Invoice Code</a>\n";
+  echo "<th class=FullD hidden><a href=javascript:SortTable(" . $coln++ . ",'T')>Invoice Code</a>\n";
   echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Notes</a>\n";
   echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Map</a>\n";
   echo "<th><a href=javascript:SortTable(" . $coln++ . ",'T')>Map Scale</a>\n";
@@ -44,15 +45,15 @@
   echo "</thead><tbody>";
   foreach($Locs as $t) {
     $i = $t['TLocId'];
-    echo "<tr><td>$i" . fm_text1("",$t,'SN',1,'','',"SN$i");
+    echo "<tr " . ($t['InUse']?'': " class=FullD hidden") . "><td>$i" . fm_text1("",$t,'SN',1,'','',"SN$i");
     echo "<td>" . fm_select($Prefixes,$t,"prefix",0,'',"prefix$i");
     echo "<td>" . fm_checkbox('',$t,'HasPower','',"HasPower$i");
     echo "<td>" . fm_checkbox('',$t,'NoList','',"NoList$i");
 //    echo fm_text1('',$t,'Pitches',0.25,'','',"Pitches$i");
     echo "<td>" . fm_checkbox('',$t,'InUse','',"InUse$i");
-    echo "<td>" . fm_select($Trade_Days,$t,"Days",0,'',"Days$i");
+    echo "<td class=FullD hidden>" . fm_select($Trade_Days,$t,"Days",0,'',"Days$i");
     echo "<td>" . fm_checkbox("",$t,'ArtisanMsgs','',"ArtisanMsgs$i");
-    echo "<td>" . fm_select($InvCodes,$t,'InvoiceCode',1,'',"InvoiceCode$i");
+    echo "<td class=FullD hidden>" . fm_select($InvCodes,$t,'InvoiceCode',1,'',"InvoiceCode$i");
     echo fm_text1('',$t,'Notes',1,'','',"Notes$i");
     echo fm_text1('',$t,'MapImage',1,'','',"MapImage$i");
     echo fm_text1('',$t,'Mapscale',0.5,'','',"Mapscale$i");
@@ -67,9 +68,9 @@
 //  echo fm_text1('',$t,'Pitches',0.25,'','',"Pitches0");
   echo "<td><input type=checkbox name=NoList0>";
   echo "<td><input type=checkbox name=InUse0>";
-  echo "<td>" . fm_select2($Trade_Days,0,'Days0');
+  echo "<td class=FullD hidden>" . fm_select2($Trade_Days,0,'Days0');
   echo "<td><input type=checkbox name=ArtisanMsgs0>";
-  echo "<td>" . fm_select($InvCodes,$t,'InvoiceCode',1,'',"InvoiceCode0");
+  echo "<td class=FullD hidden>" . fm_select($InvCodes,$t,'InvoiceCode',1,'',"InvoiceCode0");
   echo "<td><input type=text name=Notes0 >";
   echo "<td><input type=text name=MapImage0 >";
   echo fm_text1('',$t,'Mapscale',0.5,'','',"Mapscale0"); 
